0031182318b1d618d33cbf5d06e9a095493fda1e
alvaro-o <alvarortega.mil@gmail.com>
2025-04-04 10:55:39 +0200

    tests

--- src/module_6/tests/test_app.py
+++ src/module_6/tests/test_app.py
@@ -1,22 +1,7 @@
 from fastapi.testclient import TestClient
 import pytest
-from unittest.mock import Mock
-import requests
 
-from app import app, UserNotFoundException, PredictionException, FeatureStore
-
-
-class MockResponse:
-    def __init__(self, json_data, status_code):
-        self.json_data = json_data
-        self.status_code = status_code
-
-    def json(self):
-        return self.json_data
-    
-    def raise_for_status(self):
-        if self.status_code != 200:
-            raise requests.exceptions.HTTPError(f'HTTPError: {self.status_code}') 
+from app import app, FeatureStore
 
 
 client = TestClient(app)
